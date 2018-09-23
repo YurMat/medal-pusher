e5ae9a24f71dafd36cecad765f23de5c2f333583
YurMat <yurimatsu28@gmail.com>
2018-09-24 07:48:53 +0900

    create medalinit script

---
+++ Assets/Script/medalinit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class medalinit : MonoBehaviour {
+
+    public GameObject medalObjPrefab;
+    public GameObject medalsObj;
+
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("Start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 offs = new Vector3(Mathf.Sin(Time.time), 2, 0);
+            Debug.Log("Input.GetButtonDown Fire1");
+            Instantiate(medalObjPrefab, transform.position + offs, transform.rotation);
+        }
+
+    }
+}
---
+++ Assets/Script/movingstage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage_ : MonoBehaviour {
+
+    //台の移動するスピード
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
